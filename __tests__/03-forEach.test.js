417c109582fba7f8e735e02c3901aa73e9e72217
HaroldF415 <harold.f415@gmail.com>
2022-11-23 12:44:29 -0500

    testing for .forEach() complete.

--- __tests__/03-forEach.test.js
+++ __tests__/03-forEach.test.js
@@ -5,6 +5,7 @@ const {
 } = require("../src/03-forEach");
 
 const { nums, words } = require("../data/data.js");
+const songs = require("../data/songs.js");
 
 console.log(`
 .--.
@@ -15,7 +16,7 @@ console.log(`
 |__|~')___________________('
 `)
 
-describe.only("forEachWordOver4Count()", () =>{
+describe("forEachWordOver4Count()", () =>{
 
     test("should use the `.forEach()` method", () => {
         const hasForEach = !!forEachWordOver4Count.toString().match(/\.forEach(\s*\(|\()/g);
@@ -38,20 +39,63 @@ describe.only("forEachWordOver4Count()", () =>{
 
 describe("forEachWordWithExclamation()", () =>{
 
+    beforeEach(() => {
+      jest.resetModules();
+      console.log = jest.fn();
+    });
+
     test("should use the `.forEach()` method", () => {
         const hasForEach = !!forEachWordWithExclamation.toString().match(/\.forEach(\s*\(|\()/g);
         expect(hasForEach).toBeTruthy();
     });
 
-    //test("should ")
+    test("should log all words with '!' at the end of it", () => {
+
+        forEachWordWithExclamation(words);
+
+        expect(console.log).toHaveBeenCalledWith("the!");
+        expect(console.log).toHaveBeenCalledWith("quick!");
+        expect(console.log).toHaveBeenCalledWith("brown!");
+        expect(console.log).toHaveBeenCalledWith("fox!");
+        expect(console.log).toHaveBeenCalledWith("jumps!");
+        expect(console.log).toHaveBeenCalledWith("over!");
+        expect(console.log).toHaveBeenCalledWith("the!");
+        expect(console.log).toHaveBeenCalledWith("lazy!");
+        expect(console.log).toHaveBeenCalledWith("dog!");
+
+    });
+
 });
 
 describe("forEachSongPrintDetails()", () =>{
+
+    beforeEach(() => {
+        jest.resetModules();
+        console.log = jest.fn();
+    });
 
     test("should use the `.forEach()` method", () => {
         const hasForEach = !!forEachSongPrintDetails.toString().match(/\.forEach(\s*\(|\()/g);
         expect(hasForEach).toBeTruthy();
     });
+
+    test("should print both the title and artist", () => {
+        forEachSongPrintDetails(songs);
+    
+        expect(console.log).toHaveBeenCalledWith("Berlin Tsukin by Taiyo Ky");
+        expect(console.log).toHaveBeenCalledWith("Up by Sebastian Kamae");
+        expect(console.log).toHaveBeenCalledWith("Daylight by hiyasu");
+        expect(console.log).toHaveBeenCalledWith("Dormancy by Arcade Portal");
+        expect(console.log).toHaveBeenCalledWith("Pineapple Jam by Saib");
+        expect(console.log).toHaveBeenCalledWith("Samui Sunrise by Saib");
+        expect(console.log).toHaveBeenCalledWith("Regular Guy by Peanut");
+        expect(console.log).toHaveBeenCalledWith(
+          "In the Middle of Nowhere by Soulaced"
+        );
+        expect(console.log).toHaveBeenCalledWith("Pink Elephants by Timestreet");
+        expect(console.log).toHaveBeenCalledWith("Kyona by Two Sleepy");
+      });
+
 });
 
 
--- src/03-forEach.js
+++ src/03-forEach.js
@@ -6,17 +6,9 @@ const { nums, words } = require("../data/data");
 
 const forEachWordOver4Count = ( words ) => {}; 
   
-const forEachWordWithExclamation = ( words ) => {
-  
-  return words.forEach( word => console.log( `${word}!` ) );
+const forEachWordWithExclamation = ( words ) => {};
 
-};
-
-const forEachSongPrintDetails = ( songs ) => {
-
-  // return songs.forEach( song => console.log( `${song.title} by ${song.artist}` ) );
-
-};
+const forEachSongPrintDetails = ( songs ) => {};
 
 module.exports = {
 
