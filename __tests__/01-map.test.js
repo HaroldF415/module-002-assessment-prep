a1a1bf9dd838210bc6d715c641b9045303e135f2
HaroldF415 <harold.f415@gmail.com>
2022-11-25 23:36:42 -0500

    monitor logo added.

--- __tests__/01-map.test.js
+++ __tests__/01-map.test.js
@@ -11,7 +11,7 @@ const {
 
 const { nums, words } = require("../data/data.js");
 const pokemon = require("../data/poke");
-const shuffledPokemon = require("../data/poke_remix");
+const shuffledPokemon = require("../data/poke_remix"); 
 
 console.log(`
 .--.
